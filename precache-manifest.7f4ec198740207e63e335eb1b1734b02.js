cd5e7949f6669873e78191c7299b51a5e55c4965
Dhiko <dhikojjp@gmail.com>
2020-06-16 12:31:55 +0700

    deploy

---
+++ precache-manifest.7f4ec198740207e63e335eb1b1734b02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8032f81ad3c9e25a5aa",
+    "url": "/static/css/main.84626b9f.chunk.css"
+  },
+  {
+    "revision": "e8032f81ad3c9e25a5aa",
+    "url": "/static/js/main.e8032f81.chunk.js"
+  },
+  {
+    "revision": "43a3c39cbdcc9e4949f3",
+    "url": "/static/js/1.43a3c39c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6079639cc7a3252d23ab81b17c73dc90",
+    "url": "/index.html"
+  }
+];
